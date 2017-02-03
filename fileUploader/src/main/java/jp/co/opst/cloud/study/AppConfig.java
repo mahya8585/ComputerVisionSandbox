ba22a9d4ed7ca86a99c35b6f4c86af06ba8fe23e
maaya <4605851@INET-ASP.GGNET.JP>
2017-02-03 17:45:05 +0900

    ディレクトリ構成の変更

---
+++ fileUploader/src/main/java/jp/co/opst/cloud/study/AppConfig.java
@@ -0,0 +1,37 @@
+package jp.co.opst.cloud.study;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import javax.sql.DataSource;
+
+/**
+ * Config
+ * Created by maaya
+ */
+@Configuration
+public class AppConfig {
+
+    DataSource dataSource;
+
+    /**
+     * リクエストのエンコーディングをUTF-8にします。
+     */
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+    }
+
+    @Bean
+    public static ConversionService conversionService() {
+        ConversionService conversionService = new DefaultFormattingConversionService();
+        return conversionService;
+    }
+}
