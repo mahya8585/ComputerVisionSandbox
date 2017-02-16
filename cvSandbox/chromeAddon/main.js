d3f7768b1a4603a6b64a47d6e4327ae76fa8d844
maaya <4605851@INET-ASP.GGNET.JP>
2017-02-16 09:53:50 +0900

    addonするためにこれからやらねばならないことをメモ

--- cvSandbox/chromeAddon/main.js
+++ cvSandbox/chromeAddon/main.js
@@ -1,15 +1,18 @@
 $(function(){
 	//img情報取得
 	var list = document.images;
-	console.log(list);
+	//console.log(list);
+
 	$.each(list, function(i, val) {
+	    //TODO 画像1枚ずつAPIを投げる
+	    //TODO 差し替え画像URLが返却されたらhtml.replaceする
         console.log(val.src);
     });
 
 	// ページ内を加工するテスト
 
 //	$('body').html(
-//		$('body').html().replace( /ー/g, "━━━(ﾟ∀ﾟ)━━━" )
+//		$('body').html().replace( /!/g, "(ﾟ∀ﾟ)!!!" )
 //	);
 
 });
